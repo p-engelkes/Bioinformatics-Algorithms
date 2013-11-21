987cbaeb3f0538d0344fdfa0e84ed27ba1427717
Patrick Engelkes <patrick.engelkes@gmail.com>
2013-11-21 23:41:16 +0100

    -convolutionSpectrum working

--- Assignments/src/main/java/week2/ConvolutionSpectrum.java
+++ Assignments/src/main/java/week2/ConvolutionSpectrum.java
@@ -7,6 +7,7 @@ package week2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -17,70 +18,99 @@ import java.util.StringTokenizer;
 public class ConvolutionSpectrum {
 
     String sequence;
-    List<Integer> sequenceList = new ArrayList();
+    List<Integer> sequenceList;
 
     public ConvolutionSpectrum(String sequence) {
         this.sequence = sequence;
-        StringTokenizer st = new StringTokenizer(sequence);
-        while (st.hasMoreElements()) {
-            sequenceList.add(Integer.valueOf((String) st.nextElement()));
-        }
-        Integer[] array = sequenceList.toArray(new Integer[this.sequenceList.size()]);
-        Arrays.sort(array);
-        this.sequenceList.clear();
-        this.sequenceList = Arrays.asList(array);
+        sequenceList = stringToList(this.sequence);
+
     }
 
-    public List<List<Integer>> getConvolutionSpectrum() {
-        List<List<Integer>> possibleConvolutions = calculateConvolutions();
+    public List<Integer> getConvolutionSpectrum() {
+        List<Integer> convolutionSpectrum;
+        List<List<Integer>> leaderboard;
+        List<List<Integer>> sortedLeaderboard;
+        int sequenceListSize = this.sequenceList.size();
+        int[][] convolution = new int[sequenceListSize][sequenceListSize];
 
-//        List<Integer> multiplicities = calculateMultiplicities(possibleConvolutions);
-//
-//        return multiplicities;
+        convolution = calculateConvolution(this.sequenceList);
+        leaderboard = countConvolution(convolution);
+        sortedLeaderboard = sort(leaderboard);
         
-        return possibleConvolutions;
+        convolutionSpectrum = calculateConvolutionSpectrum(sortedLeaderboard);
+        
+        return convolutionSpectrum;
+    }
+    
+    public List<Integer> calculateConvolutionSpectrum(List<List<Integer>> sortedLeaderboard) {
+        List<Integer> convolutionSpectrum = new ArrayList();
+        
+        for (List<Integer> list : sortedLeaderboard) {
+            int count = list.get(1);
+            for (int i = 0; i < count; i++) {
+                convolutionSpectrum.add(list.get(0));
+            }
+        }
+        
+        return convolutionSpectrum;
+    } 
+    
+    public List<List<Integer>> sort(List<List<Integer>> listToSort) {
+        int listSize = listToSort.size();
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for (int i = 0; i < listSize - 1; i++) {
+                int one = listToSort.get(i).get(1);
+                int two = listToSort.get(i + 1).get(1);
+                if (one < two) {
+                    Collections.swap(listToSort, i, i + 1);
+                    swapped = true;
+                }
+            }
+        } while (swapped == true);
+
+        return listToSort;
     }
 
-    public List<List<Integer>> calculateConvolutions() {
-        int sequenceSize = this.sequenceList.size();
-        List<List<Integer>> possibleConvolutions = new ArrayList();
-        for (int i = 1; i < sequenceSize; i++) {
-            List<Integer> step = new ArrayList();
-            for (int k = 0; k < i; k++) {
-                int one = sequenceList.get(i);
-                int two = sequenceList.get(k);
-                step.add(one - two);
+    public List<List<Integer>> countConvolution(int[][] convolution) {
+        List<List<Integer>> leaderboard = new ArrayList();
+        for (int[] row : convolution) {
+            for (Integer i : row) {
+                List<Integer> leaderboardHelpList = new ArrayList();
+                if (!existsInList(i, leaderboard) && i > 0) {
+                    leaderboardHelpList.add(i);
+                    leaderboardHelpList.add(count(i, convolution));
+                }
+                if (!leaderboardHelpList.isEmpty()) {
+                    leaderboard.add(leaderboardHelpList);
+                }
+                
+
             }
-            possibleConvolutions.add(step);
         }
 
-        return possibleConvolutions;
+        return leaderboard;
     }
 
-    public List<Integer> calculateMultiplicities(List<List<Integer>> possibleConvolutions) {
-        List<Integer> alreadyCounted = new ArrayList();
-        List<Integer> multiplicities = new ArrayList();
-        for (List<Integer> list : possibleConvolutions) {
-            int i = 0;
-            boolean counted = false;
-            while (counted == false || i == possibleConvolutions.size()) {
-                int intToCount = list.get(i);
-                if (!existsInList(intToCount, alreadyCounted)) {
-                    count(intToCount, possibleConvolutions);
-                    counted = true;
-                    alreadyCounted.add(intToCount);
-                    multiplicities.add(intToCount);
-                } else {
-                    i++;
+    public int count(int intToCount, int[][] convolution) {
+        int count = 0;
+        for (int[] row : convolution) {
+            for (Integer i : row) {
+                if (i.equals(intToCount)) {
+                    count++;
                 }
             }
         }
-        return multiplicities;
+
+        return count;
     }
 
-    private boolean existsInList(int intToFind, List<Integer> listToCompare) {
-        for (Integer i : listToCompare) {
-            if (i.equals(intToFind)) {
+    public boolean existsInList(int intToFind, List<List<Integer>> listToCheck) {
+        for (List<Integer> list : listToCheck) {
+            int intToCompare = list.get(0);
+            if (intToCompare == intToFind) {
                 return true;
             }
         }
@@ -88,15 +118,32 @@ public class ConvolutionSpectrum {
         return false;
     }
 
-    private int count(int intToCount, List<List<Integer>> possibleConvolutions) {
-        int count = 0;
-        for (List<Integer> list : possibleConvolutions) {
-            for (Integer i : list) {
-                if (i.equals(intToCount)) {
-                    count++;
-                }
+    public int[][] calculateConvolution(List<Integer> listToCalculate) {
+        int listSize = listToCalculate.size();
+        int[][] convolution = new int[listSize][listSize];
+
+        for (int i = 0; i < listSize; i++) {
+            int subtractFrom = listToCalculate.get(i);
+            for (int k = 0; k < listSize; k++) {
+                int subtract = listToCalculate.get(k);
+                int mass = subtractFrom - subtract;
+                convolution[i][k] = mass;
             }
         }
-        return count;
+
+        return convolution;
     }
+
+    public List<Integer> stringToList(String sequence) {
+        List<Integer> list = new ArrayList();
+        StringTokenizer stringTokenizer = new StringTokenizer(sequence);
+
+        while (stringTokenizer.hasMoreElements()) {
+            String mass = (String) stringTokenizer.nextElement();
+            list.add(Integer.valueOf(mass));
+        }
+
+        return list;
+    }
+
 }
