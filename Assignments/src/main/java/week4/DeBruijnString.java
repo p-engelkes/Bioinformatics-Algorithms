5f212b474a7a14ee8a314006352bf95e037b7e8a
Patrick Engelkes <patrick.engelkes@gmail.com>
2013-12-06 12:36:55 +0100

    -started DeBruijnString problem

---
+++ Assignments/src/main/java/week4/DeBruijnString.java
@@ -0,0 +1,7 @@
+package week4;
+
+/**
+ * Created by Patrick on 06.12.13.
+ */
+public class DeBruijnString {
+}
---
+++ Assignments/src/main/java/week4/DeBruijnStringTest.java
@@ -0,0 +1,23 @@
+package week4;
+
+import org.junit.Test;
+
+/**
+ * Created by Patrick on 06.12.13.
+ */
+public class DeBruijnStringTest {
+    @Test
+    public void testGetDeBruijnGraph() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveMultipleValues() throws Exception {
+
+    }
+
+    @Test
+    public void testExistsInListMultipleTimes() throws Exception {
+
+    }
+}
