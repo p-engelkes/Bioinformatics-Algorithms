d805168b62ff214c6c8f3b8a813850b11f61788f
Patrick Engelkes <patrick.engelkes@gmail.com>
2013-12-16 18:40:20 +0100

    commit unneded changes

--- Assignments/src/main/java/week4/DeBruijnString.java
+++ Assignments/src/main/java/week4/DeBruijnString.java
@@ -1,7 +1,75 @@
 package week4;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by Patrick on 06.12.13.
  */
 public class DeBruijnString {
+
+    String sequence;
+    int k;
+
+    public DeBruijnString(String sequence, int k) {
+        this.sequence = sequence;
+        this.k = k;
+    }
+
+    public HashMap<String, List<String>> getDeBruijnGraph() {
+        HashMap<String, List<String>> deBruijnGraph = new HashMap<String, List<String>>();
+        StringReconstruction stringReconstruction = new StringReconstruction("", k - 1);
+        List<String> allKMers = stringReconstruction.getAllKMers(this.sequence);
+        List<String> kMersWithoutMultipleValues = removeMultipleValues(allKMers);
+        calculateDeBruijnGraph(kMersWithoutMultipleValues);
+
+
+
+        return deBruijnGraph;
+    }
+
+    public HashMap<String, List<String>> calculateDeBruijnGraph(List<String> kMersWithiutMultipleValues) {
+        HashMap<String, List<String>> deBruijnGraph = new HashMap<String, List<String>>();
+
+
+
+        return deBruijnGraph;
+    }
+
+    public List<String> removeMultipleValues(List<String> allKMers) {
+        StringReconstruction stringReconstruction = new StringReconstruction("", 5);
+        List<String> listWithoutMultipleValues = new ArrayList<String>(allKMers);
+        for (String s : allKMers) {
+            if (existsInListMultipleTimes(s, listWithoutMultipleValues)) {
+                listWithoutMultipleValues.remove(s);
+            }
+        }
+
+        return stringReconstruction.sortLexicographic(listWithoutMultipleValues);
+    }
+
+    public boolean existsInListMultipleTimes(String stringToCount, List<String> allKMers) {
+        int count = 0;
+
+        for (String s : allKMers) {
+            if (s.equals(stringToCount)) {
+                count++;
+            }
+        }
+
+        if (count > 1) {
+            return true;
+        }
+
+        return false;
+    }
 }
+
+
+//Erstellen sie ein Use-Case Diagramm zur Schule
+
+//Erstellen sie ein Klassendiagramm mit Unterklassen zum Thema Auto
+
+//Unterscheiden sie zwischen Komposition und Aggregation und machen sie dies an einem Beispiel deutlich.
--- Assignments/src/main/java/week4/DeBruijnStringTest.java
+++ Assignments/src/main/java/week4/DeBruijnStringTest.java
@@ -2,10 +2,21 @@ package week4;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Patrick on 06.12.13.
  */
 public class DeBruijnStringTest {
+
+    String sequence = "AAGATTCTCTAC";
+    int k = 4;
+    DeBruijnString deBruijnString = new DeBruijnString(sequence, k);
+
     @Test
     public void testGetDeBruijnGraph() throws Exception {
 
@@ -13,11 +24,32 @@ public class DeBruijnStringTest {
 
     @Test
     public void testRemoveMultipleValues() throws Exception {
+        List<String> listToCalculate = new ArrayList<String>();
+        listToCalculate.add("AAG");
+        listToCalculate.add("ACT");
+        listToCalculate.add("ACT");
+        listToCalculate.add("AAG");
+        listToCalculate.add("AGA");
 
+        List<String> expected = new ArrayList<String>();
+        expected.add("AAG");
+        expected.add("ACT");
+        expected.add("AGA");
+
+        assertEquals(expected, deBruijnString.removeMultipleValues(listToCalculate));
     }
 
     @Test
     public void testExistsInListMultipleTimes() throws Exception {
+        List<String> listToCalculate = new ArrayList<String>();
+        listToCalculate.add("AAG");
+        listToCalculate.add("ACT");
+        listToCalculate.add("ACT");
+        listToCalculate.add("AAG");
+        listToCalculate.add("AGA");
 
+        assertTrue(deBruijnString.existsInListMultipleTimes("AAG", listToCalculate));
+        assertTrue(deBruijnString.existsInListMultipleTimes("ACT", listToCalculate));
+        assertTrue(!deBruijnString.existsInListMultipleTimes("AGA", listToCalculate));
     }
 }
